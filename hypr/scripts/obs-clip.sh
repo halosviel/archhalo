b3def91fc4b73a4090a53f05442d3538607fb0dd
halosviel <guzman.carlo006@gmail.com>
2026-06-07 13:01:35 +0100

    added random icons

--- hypr/scripts/obs-clip.sh
+++ hypr/scripts/obs-clip.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 obs-cmd --websocket obsws://localhost:4455/slg20Z55ZmFTHX8G trigger-hotkey ReplayBuffer.Save
-notify-send "OBS" "Clip saved! (last 2 minutes)" -t 3000
+ICON=$(ls /home/halosviel/Local/Rice/Icons/Success/*.png | shuf -n 1)
+notify-send "OBS" "Clip saved! (last 2 minutes)" -t 3000 -i "$ICON"
