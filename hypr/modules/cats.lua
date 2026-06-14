b72bb3fced3fe38fe1db37ba3dd1a4aba8370e85
halosviel <guzman.carlo006@gmail.com>
2026-06-14 22:44:41 +0100

    update

--- hypr/modules/cats.lua
+++ hypr/modules/cats.lua
@@ -1,7 +1,64 @@
 --# cats.lua | 2026 June 14
 
+--[[
+	"Cats" is short for categories (variables).
+	See: https://wiki.hypr.land/Configuring/Basics/Variables/#syntax
+]]
+
+--# ───────────────────────────
+--# variables
+--# ───────────────────────────
+
 hl.config({
-    dwindle = {
-        preserve_split = true, -- You probably want this
-    },
+	general = {
+  	resize_on_border = true,
+  	gaps_out = { top = 10, left = 16, right = 16, bottom = 16 },
+		gaps_workspaces = -16,
+		locale = "en_US"
+	},
+
+	decoration = {
+  	rounding = 8,
+		active_opacity = 1,
+		inactive_opacity = 0.8,
+	},
+
+	input = {
+		repeat_delay = 200,
+  	repeat_rate = 40,
+
+  	natural_scroll = true,
+  	scroll_factor = 0.8,
+  	mouse_refocus = false
+	},
+
+	misc = {
+		disable_splash_rendering = true,
+		vrr = 0,
+		middle_click_paste = false,
+  	exit_window_retains_fullscreen = true
+	},
+})
+
+
+--# ───────────────────────────
+--# animations
+--# ───────────────────────────
+
+--# still counting these as variables (they used to be).
+
+hl.curve("custom", { type = "spring", mass = 1, stiffness = 100, dampening = 30 })
+
+hl.animation({
+	leaf = "windows",
+	enabled = true,
+	speed = 6,
+	spring = "custom"
+})
+
+hl.animation({
+	leaf = "workspaces",
+	enabled = true,
+	speed = 3,
+	spring = "custom"
 })
