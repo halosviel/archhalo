a6cce2bff0416f0faa93eff0135b068f43670c65
halosviel <guzman.carlo006@gmail.com>
2026-06-17 22:23:08 +0100

    upodated formatting

--- hypr/daemons/memory_ntf.sh
+++ hypr/daemons/memory_ntf.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# [CONFIG]
 CHECK_INTERVAL=2
+NTF_LIFETIME=5000
 
 # add or remove thresholds here!!
 declare -A TITLES
@@ -15,33 +17,37 @@ TITLES[98]="High memory usage"
 MESSAGES[98]="%d%% of ram being used\nSystem may shut down soon!"
 SOUNDS[98]="/home/halosviel/Local/Rice/Sounds/error.mp3"
 
-alert_icon() {
+# -->
+
+iconSad() {
   ls /home/halosviel/Local/Rice/Icons/Sad/*.png | shuf -n 1
 }
+
+# -->
 
 declare -A notified
 
 while true; do
-  TOTAL=$(grep MemTotal /proc/meminfo | awk '{print $2}')
-  AVAILABLE=$(grep MemAvailable /proc/meminfo | awk '{print $2}')
-  USED=$(( (TOTAL - AVAILABLE) * 100 / TOTAL )) 
+  totalMemory=$(grep MemTotal /proc/meminfo | awk '{print $2}')
+  availableMemory=$(grep MemAvailable /proc/meminfo | awk '{print $2}')
+  usedMemory=$(( (totalMemory - availableMemory) * 100 / totalMemory )) 
 
-  for THRESHOLD in "${!TITLES[@]}"; do
-    EXCLAMATIONS=$(printf '%.0s!' $(seq 1 $((RANDOM % 3 + 1))))
-    MSG=$(printf "${MESSAGES[$THRESHOLD]}" "$USED")
+  for threshold in "${!TITLES[@]}"; do
+    exclamations=$(printf '%.0s!' $(seq 1 $((RANDOM % 3 + 1))))
+    MSG=$(printf "${MESSAGES[$threshold]}" "$usedMemory")
 
-    if [ "$USED" -ge "$THRESHOLD" ] && [ "${notified[$THRESHOLD]}" != "true" ]; then
+    if [ "$usedMemory" -ge "$threshold" ] && [ "${notified[$threshold]}" != "true" ]; then
     	notify-send \
-        "${TITLES[$THRESHOLD]}$EXCLAMATIONS" \
+        "${TITLES[$threshold]}$exclamations" \
         "$MSG" \
-        -t 7000 \
-        -i "$(alert_icon)"
+        -t $NTF_LIFETIME \
+        -i "$(iconSad)"
 
-    	paplay --volume=32768 "${SOUNDS[$THRESHOLD]}" &
+    	paplay --volume=32768 "${SOUNDS[$threshold]}" &
 
-    	notified[$THRESHOLD]="true"
-    elif [ "$USED" -lt "$THRESHOLD" ]; then
-      notified[$THRESHOLD]="false"
+    	notified[$threshold]="true"
+    elif [ "$usedMemory" -lt "$threshold" ]; then
+      notified[$threshold]="false"
     fi 
   done
 
