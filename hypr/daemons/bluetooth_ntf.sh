12ad3078eac9d7ca99c5a92e4ed4afdef29728b0
halosviel <guzman.carlo006@gmail.com>
2026-06-17 22:12:31 +0100

    fixed formatting

--- hypr/daemons/bluetooth_ntf.sh
+++ hypr/daemons/bluetooth_ntf.sh
@@ -2,7 +2,9 @@
 
 # [CONFIG]
 SOUND="/home/halosviel/Local/Rice/Sounds/notify.mp3"
-SOUND_DELAY = 1
+SOUND_DELAY=1
+
+NTF_LIFETIME=4000
 
 # -->
 
@@ -24,18 +26,28 @@ device_name() {
 stdbuf -oL bluetoothctl | while read -r line; do
   case "$line" in
     *"Connected: yes"*)
-      MAC=$(echo "$line" | grep -oE '([0-9A-F]{2}:){5}[0-9A-F]{2}')
-      NAME=$(device_name "$MAC")
-      EXCLAMATIONS=$(printf '%.0s!' $(seq 1 $((RANDOM % 3 + 1))))
-      notify-send "Bluetooth connected$EXCLAMATIONS" "${NAME:-$MAC}" -t 7000 -i "$(happy_icon)"
+			# get device info
+      deviceMacAddress=$(echo "$line" | grep -oE '([0-9A-F]{2}:){5}[0-9A-F]{2}')
+      deviceName=$(device_name "$deviceMacAddress")
+
+			# send notification
+      exclamations=$(printf '%.0s!' $(seq 1 $((RANDOM % 3 + 1))))
+      notify-send "Bluetooth connected$exclamations" "${deviceName:-$deviceMacAddress}" -t $NTF_LIFETIME -i "$(happy_icon)"
+
+			# play sound (delayed)
 			sleep $SOUND_DELAY
       paplay --volume=32768 "$SOUND" &
       ;;
     *"Connected: no"*)
-      MAC=$(echo "$line" | grep -oE '([0-9A-F]{2}:){5}[0-9A-F]{2}')
-      NAME=$(device_name "$MAC")
-      EXCLAMATIONS=$(printf '%.0s!' $(seq 1 $((RANDOM % 3 + 1))))
-      notify-send "Bluetooth disconnected$EXCLAMATIONS" "${NAME:-$MAC}" -t 7000 -i "$(sad_icon)"
+			# get device info
+      deviceMacAddress=$(echo "$line" | grep -oE '([0-9A-F]{2}:){5}[0-9A-F]{2}')
+      deviceName=$(device_name "$deviceMacAddress")
+
+			# send notification
+      exclamations=$(printf '%.0s!' $(seq 1 $((RANDOM % 3 + 1))))
+      notify-send "Bluetooth disconnected$exclamations" "${deviceName:-$deviceMacAddress}" -t $NTF_LIFETIME -i "$(sad_icon)"
+
+			# play sound (delayed)
 			sleep $SOUND_DELAY
       paplay --volume=32768 "$SOUND" &
       ;;
