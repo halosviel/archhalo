58d26624cfad0ccd2b9fc9ad1f8db41d268395b2
halosviel <guzman.carlo006@gmail.com>
2026-06-19 17:32:03 +0100

    added window rules for Google sign-ins (windowrules don't work)

--- hypr/modules/priority.lua
+++ hypr/modules/priority.lua
@@ -13,3 +13,12 @@
 hl.on("hyprland.start", function()
 	hl.exec_cmd("systemctl --user start ags.service")
 end)
+
+--# style for sign-in prompt (rules don't work)
+hl.on("window.title", function(w)
+  if w.class == "firefox" and w.title:match("Sign in %- Google Accounts") then
+    hl.dispatch(hl.dsp.window.float({ action = "enable", window = w }))
+    hl.dispatch(hl.dsp.window.resize({ x = 400, y = 500, window = w }))
+    hl.dispatch(hl.dsp.window.center({ window = w }))
+  end
+end)
